43cb47ace255ee083b7c1067fd3cc8b33e90b9b0
Sayaka <sayaka.ssk30@gmail.com>
2025-11-02 08:52:02 +0900

    ２回目のコミット

--- XcodeGit/ContentView.swift
+++ XcodeGit/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("はじめてのコミット")
+            Text("２回目のコミット？")
             
         }
         .padding()
