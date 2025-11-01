8f7a2a118016a6abe0452a4f091c6050e70eb765
Sayaka <sayaka.ssk30@gmail.com>
2025-11-01 18:14:36 +0900

    Initial Commit

---
+++ XcodeGit/XcodeGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeGitApp.swift
+//  XcodeGit
+//
+//  Created by Sayaka Sasaki on 2025/11/01.
+//
+
+import SwiftUI
+
+@main
+struct XcodeGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
